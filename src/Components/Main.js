ee174fe42880497d38627393c3b2a2268a47ed5e
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-01 16:35:25 +0530

    started learning react

---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+
+function Main() {
+    const appName = "Texter"
+    return (
+        <main>
+            <div className="container">
+                <h1 id="heading">
+                    Welcome to {appName}
+                </h1>
+                <div className="textBox" id='textBox'>
+
+                </div>
+            </div>
+        </main>
+    ) ;
+}
+
+export default Main ;
